036896c8f01db662ba393ee3ba82323f2898848d
Sonakshi1901 <sonakshi1901@gmail.com>
2021-07-26 13:03:30 +0400

    Mobile navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
-import { Navbar, Logo, NavList, Resume, MenuIcon } from "./Navbar.styles";
+import { Navbar, Logo, NavList, Resume, List } from "./Navbar.styles";
 
 function Nav() {
   const [click, setClick] = useState(false);
@@ -28,32 +28,34 @@ function Nav() {
         <Logo to="/" onClick={closeMobileMenu}>
           Padmaja
         </Logo>
-        <MenuIcon onClick={handleClick}>
+        <NavList>
+        <div className="menu-icon" onClick={handleClick}>
           <i style={{ color:'#fff' }} className={click ? 'fas fa-times' : 'fas fa-bars'}/>
-        </MenuIcon>
-        <NavList className={click ? 'nav-menu active' : 'nav-menu'}>
-          <ul>
-            <Link to='/about' onClick={closeMobileMenu}>
+        </div>
+        <List className={click ? 'nav-menu active' : 'nav-menu'}>
+          <ul className="nav-item">
+            <Link to='/about'className="nav-links" onClick={closeMobileMenu}>
               <span className="text-white">01.</span>About
             </Link>
           </ul>
-          <ul>
-            <Link to='/projects' onClick={closeMobileMenu}>
+          <ul className="nav-item">
+            <Link to='/projects'className="nav-links" onClick={closeMobileMenu}>
               <span className="text-white">02.</span>Projects
             </Link>
           </ul>
-          <ul>
-            <Link to='/experience' onClick={closeMobileMenu}>
+          <ul className="nav-item">
+            <Link to='/experience'className="nav-links" onClick={closeMobileMenu}>
               <span className="text-white">03.</span>Experience
             </Link>
           </ul>
-          <ul>
-            <Link to='/contact' className="nav-links-mobile" onClick={closeMobileMenu}>
+          <ul className="nav-item">
+            <Link to='/contact' className="nav-links" onClick={closeMobileMenu}>
               <span className="text-white">04.</span>Contact
             </Link>
           </ul>
         {button && <Resume onClick={showButton}>RESUME</Resume>}
-        </NavList>
+        </List>
+      </NavList>
   </Navbar>
   );
 }
--- src/components/Navbar/Navbar.styles.js
+++ src/components/Navbar/Navbar.styles.js
@@ -4,12 +4,25 @@ import tw from "twin.macro";
 const Navbar = styled.div`
   ${tw`
      flex
-     grid grid-cols-8
      justify-between
      text-white
      ml-16
      justify-items-center
 `}
+
+ @media only screen and (min-width: 960px) {
+   .fa-bars {
+  color: #fff;
+  }
+
+  .nav-links-mobile {
+    display: none;
+  }
+
+  .menu-icon {
+    display: none;
+  }
+ }
 `;
 
 const Logo = styled.a`
@@ -19,6 +32,12 @@ const Logo = styled.a`
    sm:text-5xl
    text-primary
 `}
+ @media screen and (max-width: 960px) {
+     position: absolute;
+     font-size: 3rem;
+     left: 0;
+     transform: translate(10%, 6%);
+ }
 `;
 
 const NavList = styled.div`
@@ -32,19 +51,88 @@ const NavList = styled.div`
      font-roboto
      uppercase
      font-medium
-     col-start-6
-     col-end-8
 `}
+
+ @media screen and (max-width: 960px) {
+  /* .NavbarItems {
+    position: relative;
+  } */
+
+  .nav-menu{
+    display: flex;
+    flex-direction: column;
+    width: 100%;
+    height: 90vh;
+    position: absolute;
+    top: 80px;
+    left: -100%;
+    opacity: 1;
+    transition: all 0.5s ease;
+  }
+
+  .nav-menu.active {
+    background: black;
+    left: 0;
+    opacity: 1;
+    overflow-x: hidden;
+    overflow-y: hidden;
+    transition: all 0.5s ease;
+    z-index: 1;
+  }
+
+  .nav-links {
+    text-align: center;
+    padding: 1rem;
+    width: 100%;
+    display: table;
+  }
+
+  .nav-links:hover {
+  }
+
+  .fa-times {
+    color: #fff;
+    font-size: 2rem;
+  }
+
+  .nav-links-mobile {
+    display: block;
+    text-align: center;
+    margin: 2rem auto;
+    border-radius: 4px;
+    width: 80%;
+    text-decoration: none;
+    font-size: 1.5rem;
+    background-color: transparent;
+    color: #fff;
+    padding: 14px 20px;
+    border: 1px solid #fff;
+    transition: all 0.3s ease-out;
+  }
+
+  .menu-icon{
+    display: block;
+    position: absolute;
+    top: 0;
+    right: 0;
+    transform: translate(-100%, 60%);
+    font-size: 1.8rem;
+    cursor: pointer;
+  }
+
+  .fa-bars {
+  color: #fff;
+  font-size: 2rem;
+  }
+
 `;
 
-const MenuIcon = styled.div`
+const List = styled.ul`
   ${tw`
-   block
-   absolute
-   object-right-top
-   transform-translate(-100%, 60%);
-   text-3xl
-   cursor-pointer
+    justify-items-center
+    flex
+    gap-5
+    mr-8
 `}
 `;
 
@@ -58,4 +146,6 @@ const Resume = styled.button`
 `}
 `;
 
-export { Navbar, Logo, NavList, Resume, MenuIcon };
+
+
+export { Navbar, Logo, NavList, Resume, List };
