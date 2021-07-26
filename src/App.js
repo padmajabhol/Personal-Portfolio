282c8c2f6960f764983a1af9ab5ba28c11b42a3b
Padmaja Bhol <75530516+padmajabhol@users.noreply.github.com>
2021-07-26 18:20:33 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import "./App.css";
 import { AppContainer } from "./components/Styles/Container.style";
 import Nav from "./components/Navbar/Navbar";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { BrowserRouter as Router } from "react-router-dom";
 
 function App() {
   return (
